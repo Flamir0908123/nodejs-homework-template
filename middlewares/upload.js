4c075b9ac61f736a703faa87d5d98cf504daa567
Flamir0908123 <bgladki86@gmail.com>
2021-10-11 19:30:30 +0300

    рь

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,8 +2,7 @@ const multer = require('multer')
 const path = require('path')
 
 const tempDir = path.join(__dirname, '../temp')
-// const uploadDir = path.join(__dirname, '../public/avatars')
-// console.log(uploadDir)
+
 const uploadConfig = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, tempDir)
